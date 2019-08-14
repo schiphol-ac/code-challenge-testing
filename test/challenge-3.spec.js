fb8d0a4b87bd6ed713b08bc0596e2f47f19c777d
Hetty de Vries <hetty82@gmail.com>
2019-08-14 17:48:57 +0200

    ✅ Update challenge 3 specs

--- test/challenge-3.spec.js
+++ test/challenge-3.spec.js
@@ -1,43 +1,39 @@
-const { expect } = require('chai');
-const { diffArrays } = require('../challenge-3/challenge');
+const { expect } = require("chai");
+const { diffArrays } = require("../challenge-3/challenge");
 
-describe('Challenge 3', () => {
-  it('should return values in a and not in b', () => {
-    const a = [1, 2, 3, 4, 5];
-    const b = [5, 6, 7, 8, 9];
-    const result = diffArrays(a, b);
-    [1,2,3,4].forEach(item => {
-      expect(result).to.include(item);
-    });
-  });
-  it('should return values in b and not in a', () => {
-    const a = [1, 2, 3, 4, 5];
-    const b = [5, 6, 7, 8, 9];
-    const result = diffArrays(a, b);
-    [6,7,8,9].forEach(item => {
-      expect(result).to.include(item);
-    })
-  });
-  it('should not return values in a and b', () => {
-    const a = [1, 2, 3, 4, 5];
-    const b = [5, 6, 7, 8, 9];
-    const result = diffArrays(a, b);
-    expect(result).to.not.include(5);
-  });
-  it('should return return array 1,2,3,4,6,7,8,9', () => {
+describe("Challenge 3", () => {
+  it("should return return array 1,2,3,4,6,7,8,9", () => {
     const a = [1, 2, 3, 4, 5];
     const b = [5, 6, 7, 8, 9];
     const result = diffArrays(a, b);
     expect(result).to.deep.equal([1, 2, 3, 4, 6, 7, 8, 9]);
   });
-});
-describe('Challenge 3 additional checks', () => {
-  it('should return return mixed array with mutually exclusive values', () => {
-    const a = ['a', 'b', 'c', 6, 'd'];
-    const b = [5, 6, 'b', 7, 'd', 8, 9];
+
+  it("should return return array 1,2,3,4,5,6,7,8,9", () => {
+    const a = [1, 2, 3, 4];
+    const b = [5, 5, 6, 7, 8, 9];
     const result = diffArrays(a, b);
-    expect(result).to.deep.equal(['a', 'c', 5, 7, 8, 9]);
+    expect(result).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it("should return return array 1,2,3,5,6,7,8,9", () => {
+    const a = [1, 2, 3, 4, 4];
+    const b = [4, 5, 5, 6, 7, 8, 9];
+    const result = diffArrays(a, b);
+    expect(result).to.deep.equal([1, 2, 3, 5, 6, 7, 8, 9]);
+  });
+
+  it("should return return a mixed array with mutually exclusive values", () => {
+    const a = ["a", "b", "c", 6, "d"];
+    const b = [5, 6, "b", 7, "d", 8, 9];
+    const result = diffArrays(a, b);
+    expect(result).to.deep.equal(["a", "c", 5, 7, 8, 9]);
+  });
+
+  it("should return an empty array when all values exist in both arrays", () => {
+    const a = [1, 1, 2, 3, 4, 4];
+    const b = [1, 2, 2, 3, 3, 4, 4];
+    const result = diffArrays(a, b);
+    expect(result).to.deep.equal([]);
   });
 });
-
-
